bcb0387b37cfccedbb05b70d0f91e1e6112e158b
Aleksi Myllyoja <aleksi.myllyoja@gmail.com>
2018-09-13 04:22:44 +0300

    Faces temp save

--- faces.py
+++ faces.py
@@ -1,4 +1,6 @@
 from helpers import *
+from random import *
+from functools import reduce
 
 W = 300
 H = 218
@@ -46,6 +48,9 @@ def mouth(x, y, w, h):
 
     return [ll, ul, c]
 
+def fcircle(x, y, r, d=3.0):
+    return reduce(lambda x, y: x+y, [circle(x, y, i/3.0) for i in range(0, int(d*r+1))], [])
+
 def eye(x, y, w, h):
     hw = w/2
     hh = h/2
@@ -62,7 +67,7 @@ def eye(x, y, w, h):
         [x+hw, y]
     ])
 
-    return [ul, ll, circle(x, y, hh/2.0), circle(x, y, hh/4.0)]
+    return [ul, ll, circle(x, y, hh/2.0), fcircle(x, y, hh/4.0)]
 
 def nose(x, y, w, h):
     return bezier([
@@ -75,15 +80,75 @@ def nose(x, y, w, h):
 
 def face(x, y, r):
 
-    ew = r/2.5
+    ew = r/(randint(200, 300)/100.0) # eye width
+    eh = r/(randint(200, 480)/100.0) # eye height
 
-    paths.append(circle(x, y, r))
-    paths.append(nose(x, y+r/4.0, ew, ew/2.0))
+    jw = randint(40, 90)/100.0  # jaw width
+    jb = randint(110, 140)/100.0 # jaw bend
+    jp = randint(60, 80)/100.0 # jaw position
 
-    paths.extend(eye(x-ew, y, ew, ew/1.33))
-    paths.extend(eye(x+ew, y, ew, ew/1.33))
+    fw = jw+randint(5, 20)/100.0 # face width
 
-    paths.extend(mouth(x, y+r/1.66, ew*1.33, ew/1.8))
+    # eye distance
+    edx = (r*fw)/(randint(180, 220)/100.0)
+    edy = (random()-.5)*ew
 
-face(W/2, H/2, 100)
+    mh = ew*randint(5, 100)/100.0 # mouth height
+    mw = ew*randint(80, 200)/100.0 # mouth width
+
+    jlb = x-r*jw
+    jrb = x+r*jw
+
+    # B
+    paths.append(
+        bezier([
+            [jlb, y+r*jp],
+            [x, y+r*jb],
+            [jrb, y+r*jp]
+        ])
+    )
+
+    # L
+    paths.append(
+        bezier([
+            [jlb, y+r*jp],
+            [x-r*fw, y+r*.4],
+            [x-r*fw, y-r*.3],
+        ])
+    )
+
+    # R
+    paths.append(
+        bezier([
+            [jrb, y+r*jp],
+            [x+r*fw, y+r*.4],
+            [x+r*fw, y-r*.3],
+        ])
+    )
+
+    # H
+    paths.append(
+        bezier([
+            [x-r*fw, y-r*.3],
+            [x-r*(fw-.1), y-r*1.2],
+            [x+r*(fw-.1), y-r*1.2],
+            [x+r*fw, y-r*.3],
+        ])
+    )
+
+    paths.append(nose(x, y+r/3.0, ew/2.0, ew/5.0))
+
+    paths.extend(eye(x-edx, y+edy, ew, eh))
+    paths.extend(eye(x+edx, y+edy, ew, eh))
+
+    paths.extend(mouth(x, y+r/1.66, mw, mh))
+
+w = 30
+for j in range(3):
+    for i in range(4):
+        face(40+i*w*2.4, 40+j*w*2.2, w)
+
+#paths.append(fcircle(W/2, H/2, 10))
+#face(W/2, H/2, 100)
+
 plot_paths(paths)
